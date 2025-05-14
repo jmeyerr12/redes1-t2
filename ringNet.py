2cbf47d6c0bc0122fa63cea77ab01e37b06313cd
jmeyerr12 <joaomeyermuhlmann@gmail.com>
2025-05-14 20:20:06 -0300

    a

--- ringNet.py
+++ ringNet.py
@@ -4,17 +4,17 @@ import json
 import random
 import sys
 
-# Porta única usada por todos os jogadores
+#porta única usada por todos os jogadores
 PORT = 31204
 
-# Obtém o ID do jogador (0 a 3) passado por argumento
+#pega o ID do jogador (0 a 3) passado por argumento
 player_id = int(sys.argv[1])
 
-# Lista das cartas do baralho
+#lista das cartas do baralho
 naipes = ["ouros", "copas", "espadas", "paus"]
 cartas = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
-# Lista de IPs dos jogadores na ordem do anel
+#lista de IPs dos jogadores na ordem do anel
 players = [
     ("10.254.225.24", PORT),  # Jogador 0
     ("10.254.225.25", PORT),  # Jogador 1
@@ -22,7 +22,7 @@ players = [
     ("10.254.225.27", PORT),  # Jogador 3
 ]
 
-# Define meu IP e IP do próximo jogador no anel
+#define meu IP e IP do próximo jogador no anel
 MY_IP, MY_PORT = players[player_id]
 NEXT_IP, NEXT_PORT = players[(player_id + 1) % 4]
 
@@ -32,7 +32,7 @@ sock.bind((MY_IP, MY_PORT))
 
 print(f"Jogador {player_id} iniciado em {MY_IP}:{MY_PORT}. Aguardando bastão...")
 
-# Apenas o jogador 0 cria o bastão no início
+#apenas o jogador 0 cria o bastão no início
 if player_id == 0:
     deck = [carta + naipe for carta in cartas for naipe in naipes] #junta cada carta com um naipe
     random.shuffle(deck);
@@ -44,7 +44,7 @@ if player_id == 0:
     }
     print(f"[{player_id}] Cartas distribuídas.")
 
-    time.sleep(10)  # Espera os outros iniciarem
+    time.sleep(10)  #espera os outros iniciarem (pra garantir que da certo rode o 0 por ultimo)
     token = {
         "type": "token",
         "round": 1,
@@ -55,14 +55,13 @@ if player_id == 0:
     }
     sock.sendto(json.dumps(token).encode(), (NEXT_IP, NEXT_PORT))
 
-# Loop principal de recebimento de mensagens
+#loop principal de recebimento de mensagens
 while True:
     data, addr = sock.recvfrom(1024)
     message = json.loads(data.decode())
 
-    # Quando receber o bastão
+    #quando recebe o bastao
     if message["type"] == "token":
-        # Aqui entrará a lógica do jogo Copas
         if "hands" in message:
             my_hand = message["hands"][str(player_id)] if isinstance(message["hands"], dict) else message["hands"][player_id]
             print(f"[{player_id}] Minhas cartas: {my_hand}")
