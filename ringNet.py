9601eda01b177048e21df956fdac684651870465
jmeyerr12 <joaomeyermuhlmann@gmail.com>
2025-05-13 21:11:33 -0300

    a

--- ringNet.py
+++ ringNet.py
@@ -18,48 +18,37 @@ PROXIMO_IP, PROXIMO_PORTA = jogadores[(meu_id + 1) % 4]
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((MEU_IP, MEU_PORTA))
 
-print(f"[{meu_id}] Iniciado em {MEU_IP}:{MEU_PORTA}")
+print(f"[{meu_id}] Pronto em {MEU_IP}:{MEU_PORTA}")
 
-# Inicialização: ping-pong
+estado = "inicial"
+
 if meu_id == 0:
-    ping = {"tipo": "ping", "de": meu_id}
-    time.sleep(1)
-    sock.sendto(json.dumps(ping).encode(), (PROXIMO_IP, PROXIMO_PORTA))
+    time.sleep(2)
+    msg = {"tipo": "ping", "origem": 0}
+    sock.sendto(json.dumps(msg).encode(), (PROXIMO_IP, PROXIMO_PORTA))
+    estado = "aguardando_pong"
 
-    while True:
-        data, _ = sock.recvfrom(1024)
-        msg = json.loads(data.decode())
-
-        if msg["tipo"] == "pong":
-            print(f"[{meu_id}] Todos online. Enviando bastão.")
-            time.sleep(1)
-            token = {"tipo": "token"}
-            sock.sendto(json.dumps(token).encode(), (PROXIMO_IP, PROXIMO_PORTA))
-            break
-        else:
-            sock.sendto(data, (PROXIMO_IP, PROXIMO_PORTA))
-
-else:
-    while True:
-        data, _ = sock.recvfrom(1024)
-        msg = json.loads(data.decode())
-
-        if msg["tipo"] == "ping":
-            if meu_id == 3:
-                pong = {"tipo": "pong"}
-                sock.sendto(json.dumps(pong).encode(), (PROXIMO_IP, PROXIMO_PORTA))
-            else:
-                sock.sendto(data, (PROXIMO_IP, PROXIMO_PORTA))
-        elif msg["tipo"] == "pong":
-            sock.sendto(data, (PROXIMO_IP, PROXIMO_PORTA))
-            break
-
-# Loop principal (bastão)
 while True:
     data, _ = sock.recvfrom(1024)
     msg = json.loads(data.decode())
 
-    if msg["tipo"] == "token":
+    if msg["tipo"] == "ping":
+        if meu_id == 3:
+            resposta = {"tipo": "pong", "origem": 3}
+            sock.sendto(json.dumps(resposta).encode(), (PROXIMO_IP, PROXIMO_PORTA))
+        else:
+            sock.sendto(data, (PROXIMO_IP, PROXIMO_PORTA))
+
+    elif msg["tipo"] == "pong":
+        sock.sendto(data, (PROXIMO_IP, PROXIMO_PORTA))
+        if meu_id == 0 and estado == "aguardando_pong":
+            print(f"[{meu_id}] Todos online. Criando bastão.")
+            time.sleep(1)
+            token = {"tipo": "token"}
+            sock.sendto(json.dumps(token).encode(), (PROXIMO_IP, PROXIMO_PORTA))
+            estado = "ativo"
+
+    elif msg["tipo"] == "token":
         print(f"[{meu_id}] Recebi o bastão.")
         time.sleep(2)
         sock.sendto(json.dumps(msg).encode(), (PROXIMO_IP, PROXIMO_PORTA))
